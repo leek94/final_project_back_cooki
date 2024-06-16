61000f4950dc970a3f9451051406e12eedf8a826
leek94 <khwan929@naver.com>
2024-06-16 15:54:20 +0900

    feat: service add

--- src/main/java/com/mycompany/webapp/service/ClassService.java
+++ src/main/java/com/mycompany/webapp/service/ClassService.java
@@ -1,8 +1,15 @@
 package com.mycompany.webapp.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.mycompany.webapp.dao.ClassDao;
 
 @Service
 public class ClassService {
-
+	
+	@Autowired
+	private ClassDao classDao;
+	
+	
 }
--- src/main/java/com/mycompany/webapp/service/RecipeService.java
+++ src/main/java/com/mycompany/webapp/service/RecipeService.java
@@ -1,8 +1,13 @@
 package com.mycompany.webapp.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.mycompany.webapp.dao.RecipeDao;
 
 @Service
 public class RecipeService {
-
+	
+	@Autowired
+	private RecipeDao recipeDao;
 }
