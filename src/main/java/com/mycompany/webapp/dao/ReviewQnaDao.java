a70c222c1681aa3e98864fd9f1a6e1b352f33293
BHM <kakao940720@gmail.com>
2024-07-10 17:06:45 +0900

    Feat: classReview pagination add #38

--- src/main/java/com/mycompany/webapp/dao/ReviewQnaDao.java
+++ src/main/java/com/mycompany/webapp/dao/ReviewQnaDao.java
@@ -3,8 +3,10 @@ package com.mycompany.webapp.dao;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.mycompany.webapp.dto.ClassReview;
+import com.mycompany.webapp.dto.Pager;
 import com.mycompany.webapp.dto.Qna;
 import com.mycompany.webapp.dto.RecipeReview;
 
@@ -17,7 +19,8 @@ public interface ReviewQnaDao {
 	public int deleteQnaByQno(int qno);
 	public int updateQreplyByQno(Qna qna);
 	public int insertClassReview(ClassReview classReview);
-	public List<ClassReview> selectClassReviewByCno(int cno);
+	public int selectReviewCountByCno(int cno);
+	public List<ClassReview> selectClassReviewByCnoPager(int cno, @Param("pager") Pager pager);
 	public Float selectAvgCrratioByCno(int cno);
 	public int updateClassReviewByCrno(ClassReview classReview);
 	public int deleteClassReviewByCrno(int crno);
